6d5ce64474ca875c36c20d2d9de43b7f14cfe0ff
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-26 19:23:46 +0000

    ask for radius

--- Cilindro/Program.cs
+++ Cilindro/Program.cs
@@ -8,6 +8,9 @@ namespace Cilindro
         {
             Console.Write("Insira a altura do cilindro (a): ");
             double altura = double.Parse(Console.ReadLine());
+            
+            Console.Write("Insira o raio do cilindro (r): ");
+            double raio = double.Parse(Console.ReadLine());
         }
     }
 }
