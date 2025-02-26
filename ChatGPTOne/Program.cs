48eb5fc5d46574899fc7c74aa8a97768d0e0d596
FranciscoAnjos <fcariaanjos@gmail.com>
2025-02-26 19:55:39 +0000

    add question one

---
+++ ChatGPTOne/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ChatGPTOne
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            bool loop = true;
+
+            while (loop)
+            {
+                Console.WriteLine("Please enter a question or type 'EXIT' to quit:");
+                string question = Console.ReadLine();
+
+                if (question != null && question.Equals("EXIT", StringComparison.CurrentCultureIgnoreCase))
+                {
+                    loop = false;
+                    Console.WriteLine("Goodbye!");
+                }
+                else
+                {
+                    if (question != null && question.Equals("what is your name?", StringComparison.CurrentCultureIgnoreCase))
+                    {
+                        Console.WriteLine("I'm ChatBot!");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Sorry, I didn't recognize your question.");
+                    }
+                }
+            }
+        }
+    }
+}
